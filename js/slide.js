fc3c62081311e2f31a564cdf28f060c8f9cf45dd
Patricia <patricia@patricia-X550CC.(none)>
2014-01-27 18:23:41 +0000

    Adiciona links

--- js/slide.js
+++ js/slide.js
@@ -1,7 +1,34 @@
 slideDown = function(){
   $('.main').moveDown();
 }
+moveToSaturday = function(){
+	$('.main').moveTo(3);
+}
+
+moveToMonday = function(){
+	$('.main').moveTo(4);
+}
+
+moveToTuesday = function(){
+	$('.main').moveTo(5);
+}
+
+moveToWednesday = function(){
+	$('.main').moveTo(6);
+}
+moveToThursday = function(){
+	$('.main').moveTo(7);
+}
+moveToFriday = function(){
+	$('.main').moveTo(8);
+}
 
 $(document).ready(function(){
-  $('.slide-down').on('click', slideDown);  
+  $('.slide-down').on('click', slideDown); 
+  $('.act-saturday').on('click', moveToSaturday);
+  $('.act-monday').on('click', moveToMonday);
+  $('.act-tuesday').on('click', moveToTuesday);
+  $('.act-wednesday').on('click', moveToWednesday);
+  $('.act-thursday').on('click', moveToThursday);
+  $('.act-friday').on('click', moveToFriday);
 });
