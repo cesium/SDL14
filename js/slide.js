80576a3459764b5657de2deb03ca93a8b7eb388c
Pedro Carneiro <pedrosfdcarneiro@gmail.com>
2014-02-19 04:27:02 +0000

    removido one page

--- js/slide.js
+++ js/slide.js
@@ -2,25 +2,25 @@ slideDown = function(){
   $('.main').moveDown();
 }
 moveToSaturday = function(){
-	$('.main').moveTo(3);
-}
-
-moveToMonday = function(){
 	$('.main').moveTo(4);
 }
 
-moveToTuesday = function(){
+moveToMonday = function(){
 	$('.main').moveTo(5);
 }
 
-moveToWednesday = function(){
+moveToTuesday = function(){
 	$('.main').moveTo(6);
 }
-moveToThursday = function(){
+
+moveToWednesday = function(){
 	$('.main').moveTo(7);
 }
-moveToFriday = function(){
+moveToThursday = function(){
 	$('.main').moveTo(8);
+}
+moveToFriday = function(){
+	$('.main').moveTo(9);
 }
 
 $(document).ready(function(){
